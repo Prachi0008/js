55d7a30996068c2c5a33a1bad981ce9b6bc8ac3f
prachiSingh <120970849+Prachi0008@users.noreply.github.com>
2024-12-20 06:28:36 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "singh@gmail.com"
+var accountPassword =  "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+
+accountEmail = "abc@gmail.com"
+accountPassword = "67890"
+accountCity = "rajasthan"
+
+console.table([accountId, accountEmail, accountCity, accountPassword]);
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) //we are using node js not browser
+
+console.log(3+3)
+console.log("prachi") //maintain code readability
+
+let name = "prachi"
+let age = 23
+let isLoggedIn = false
+let state = null
+
+// number => 2 to the power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => stand alone value
+// undefined => 
+// symbol => unique
+
+//object
+
+console.log(typeof "prachi");
+console.log(typeof null); //object
