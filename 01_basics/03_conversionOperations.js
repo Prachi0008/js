084036f000156dc2135cd4c9687a437294c74ed0
prachiSingh <120970849+Prachi0008@users.noreply.github.com>
2024-12-20 08:49:24 +0000

    operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,35 @@
+let score = null
+// console.log(typeof score);
+// console.log(typeof(score)); //both are same
+
+let valueInNumber =  Number(score)
+// console.log(valueInNumber);
+
+let isLoggedIn=1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log((booleanIsLoggedIn));
+
+// 1 => true // 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// **************** OPERATIONS *****************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+console.log("1"+2); //12
+console.log(1+"2"); //12
+console.log("1"+2+2); //122
+console.log(1+2+"2"); //32
+
+
